7f5a632a136b465eb4cc89adc704b12d5598386a
Rodrigo Ferreira <rodrigo.ferreira2022@engenharia.ufjf.br>
2024-06-18 15:05:43 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,50 @@
+import serial.tools.list_ports
+import os
+
+nome = input("Digite o nome do arquivo: ").strip('\n').strip('.txt').strip() + '.txt'
+baud = 9600
+
+while os.path.exists(nome):
+    inp = input(f"O arquivo '{nome}' já existe. Deseja sobrescrever? (s/n): ").strip().lower()
+    if inp in ['n','nao', 'nn','0']:
+        nome = input("Digite o nome do arquivo: ").strip('\n').strip('txt') + '.txt'
+    elif inp in ['s','sim','1']:
+        break
+
+print()
+
+ports = serial.tools.list_ports.comports()
+serialInst = serial.Serial()
+
+portList = []
+
+if len(ports) == 0:
+    print("Nenhuma porta serial encontrada")
+    exit()
+
+for onePort in ports:
+    portList.append(str(onePort))
+    print(str(onePort))
+print()
+
+val = input("Selecione a porta: COM")
+
+for x in range(0, len(portList)):
+    if portList[x].startswith("COM" + str(val)):
+        portVar = 'COM' + str(val)
+
+serialInst.baudrate = baud
+serialInst.port = portVar
+serialInst.open()
+
+arquivo = open(nome, 'w')
+print("\nGravando dados...\nCtrl+C para parar")
+print()
+
+while 1:
+    if serialInst.in_waiting:
+        val = serialInst.readline()
+        val = val.decode("utf").strip()
+
+        arquivo.write(val+'\n')
+        print(val)
